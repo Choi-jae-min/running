a4318d672720a64fe6ae59bfa40ae8f8d0d0c913
jasmin <cjm98@smartel.co.kr>
2026-01-15 12:43:23 +0900

    day01 - step4

---
+++ src/main/java/step4/Coffee.java
@@ -0,0 +1,14 @@
+package step4;
+
+public class Coffee {
+    private final String name;
+
+    public Coffee(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "☕ " + name;
+    }
+}
---
+++ src/main/java/step4/CoffeeMaker.java
@@ -0,0 +1,6 @@
+package step4;
+
+public interface CoffeeMaker {
+
+    Coffee makeCoffee(String menuName);
+}
