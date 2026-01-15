fb4e8ad8b0de3662d16c5ceeb462fe6675cbeebe
jasmin <cjm98@smartel.co.kr>
2026-01-15 19:05:08 +0900

    day01 - step5

---
+++ src/main/java/step5/Coffee.java
@@ -0,0 +1,14 @@
+package step5;
+
+public class Coffee {
+    private final String name;
+
+    public Coffee(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "☕ " + name;
+    }
+}
---
+++ src/main/java/step5/CoffeeMaker.java
@@ -0,0 +1,6 @@
+package step5;
+
+public interface CoffeeMaker {
+
+    Coffee makeCoffee(String menuName);
+}
